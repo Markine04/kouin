10ad8dd29a0b8d42fb0a22b85f913602a8a067a6
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-11 23:42:48 +0000

    Update index.blade.php

--- resources/views/dashboard/annonce-flash/index.blade.php
+++ resources/views/dashboard/annonce-flash/index.blade.php
@@ -82,17 +82,9 @@
                     </div><!--//app-card-->
                     <nav class="app-pagination">
 
-                        {{ $Flashs->links() }}
+                        
                         <ul class="pagination justify-content-center">
-                            <li class="page-item disabled">
-                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                            </li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">Next</a>
-                            </li>
+                            {{ $Flashs->links() }}
                         </ul>
                     </nav><!--//app-pagination-->
 
