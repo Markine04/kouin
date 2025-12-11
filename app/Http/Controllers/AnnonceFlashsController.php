d8e43fd7bdd310a613b2e7ab411909cef7455553
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-11 23:26:51 +0000

    Update AnnonceFlashsController.php

--- app/Http/Controllers/AnnonceFlashsController.php
+++ app/Http/Controllers/AnnonceFlashsController.php
@@ -71,7 +71,7 @@ class AnnonceFlashsController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
         DB::table('flashers')->where('id',$request->id)->insert([
             'titre' => $request->titre,
