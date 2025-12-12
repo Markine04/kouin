f2afd0e82159209e2cb937a8c06df5a9cc6537df
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-12 11:11:07 +0000

    Update SecteursActivitesController.php

--- app/Http/Controllers/SecteursActivitesController.php
+++ app/Http/Controllers/SecteursActivitesController.php
@@ -89,7 +89,7 @@ class SecteursActivitesController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request)
     {
         // dd($request->all());
         // if ($request->is_active == 1) {
