068e805b18ccfa1ec7009b8f1ce79f10b1c92845
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-12 09:33:46 +0000

    Update index.blade.php

--- resources/views/dashboard/annonce-flash/index.blade.php
+++ resources/views/dashboard/annonce-flash/index.blade.php
@@ -37,10 +37,10 @@
             </div><!--//app-card-->
 
             <div class="row g-3 align-items-center">
-  <div class="col-lg-4 col-sm-12 col-md-12">
+  <div class="col-auto">
     <label for="inputPassword6" class="col-form-label">Password</label>
   </div>
-  <div class="col-auto">
+  <div class="col-lg-4 col-sm-12 col-md-12">
     <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
   </div>
   <div class="col-lg-4 col-sm-12 col-md-12">
