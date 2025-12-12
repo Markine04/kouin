6795bd7dbd950c6d670719c6144c51677b9b5ef8
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2025-12-12 09:31:13 +0000

    Update index.blade.php

--- resources/views/dashboard/annonce-flash/index.blade.php
+++ resources/views/dashboard/annonce-flash/index.blade.php
@@ -41,12 +41,11 @@
     <label for="inputPassword6" class="col-form-label">Password</label>
   </div>
   <div class="col-auto">
-    <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+    <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
   </div>
   <div class="col-auto">
-    <span id="passwordHelpInline" class="form-text">
-      Must be 8-20 characters long.
-    </span>
+    <input type="submit" class="btn btn-primary text-white" value="Rechercher">
+
   </div>
 </div>
             <p></p>
