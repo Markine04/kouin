fbf296bf789c937b5774072c97308db84adb62a8
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-13 18:10:01 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -18,7 +18,7 @@ class PropertyController extends Controller
     public function index(Request $request)
     {
         $page = $request->page ?? 1;
-        $perPage = $request->per_page ?? 100;
+        $perPage = $request->per_page ?? 20;
         $search = $request->search ?? null;
 
         $queryParams = [
