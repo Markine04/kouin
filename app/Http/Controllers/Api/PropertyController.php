4c90303f8814f8c1879bae1f0eaff47331b67913
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-15 14:31:14 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -123,7 +123,7 @@ class PropertyController extends Controller
 
         return response()->json([
             'data' => $formatted,
-            'a_la_une' => $a_la_une->first(),
+            'a_la_une' => $a_la_une,
         ]);
     }
 
