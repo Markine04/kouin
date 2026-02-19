ce61f4045ea8e7dd9100a815675a146cb5227902
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-19 09:09:36 +0000

    Update UserBiimController.php

--- app/Http/Controllers/Api/UserBiimController.php
+++ app/Http/Controllers/Api/UserBiimController.php
@@ -107,8 +107,6 @@ class UserBiimController extends Controller
 
     public function loginToWordpress(Request $request)
     {
-        // $essaie = 125;
-        // dd($essaie);
         $request->validate([
             'username' => 'required',
             'password' => 'required'
