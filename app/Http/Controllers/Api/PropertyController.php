3a4e6c40265be64124ab3d12ac7924176dfeef29
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-24 20:22:44 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -496,7 +496,9 @@ class PropertyController extends Controller
 
         $propertyData = [
             'title'   => $request->title,
-            'content' => $request->description,
+            'content' => 
+            // $request->description 
+            'A description is a spoken or written account that paints a vivid mental picture of a person, place, object, or event, often utilizing sensory details. It acts as one of the four main rhetorical modes, focusing o',
             'status'  => 'pending',
             'slug'    => str_replace(' ', '-', strtolower($request->title)),
             'type'    => 'property',
@@ -529,8 +531,7 @@ class PropertyController extends Controller
 
         $propertyResponse = Http::withToken(
             $token
-            // 'Content-Type' => 'application/json'
-        )->post('https://biim.ci/wp-json/wp/v2/property', json_encode($propertyData));
+        )->asJson()->post('https://biim.ci/wp-json/wp/v2/property', $propertyData);
 
         if ($propertyResponse->successful()) {
             return response()->json([
