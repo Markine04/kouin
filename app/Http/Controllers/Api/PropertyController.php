feb4b800a07504390bfc644ff17f7435b581c4ed
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-25 08:37:51 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -494,7 +494,7 @@ class PropertyController extends Controller
         $featuredMedia = $featuredMediaId; // Première image = cover
         $gallery = implode('|', $uploadedIds); // Le reste = galerie
 
-        $recupererDonnéees = [
+        $recupererDonnéees = json_encode([
             'real_estate_property_price' => $request->price ?? '',
             'real_estate_property_price_postfix' => $request->period ?? 'NUITÉE',
             'real_estate_property_address' => $request->address ?? '',
@@ -511,7 +511,7 @@ class PropertyController extends Controller
             'real_estate_property_size' => $request->area ?? '',
             'real_estate_property_land' => $request->land_area ?? '',
             'real_estate_property_bedrooms' => $request->bedrooms ?? '',
-        ];
+        ]);
         
     
         $propertyData = [
