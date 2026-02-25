2bc2cb761b4cf337e3f4d25d26df13b39704e05c
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-25 09:31:19 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -517,7 +517,7 @@ class PropertyController extends Controller
     
         $propertyData = [
             'title'   => $request->title,
-            'content' => "<p>" . htmlspecialchars($request->description) . "</p>",
+            'content' => "<p>" .$request->description. "</p>",
             // "\n<p>" . htmlspecialchars($request->description) . "</p>\n",
             // 'A description is a spoken or written account that paints a vivid mental picture of a person, place, object, or event, often utilizing sensory details. It acts as one of the four main rhetorical modes, focusing o',
             'status'  => 'pending',
