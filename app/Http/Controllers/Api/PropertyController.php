757e13b20260f8bc405a602ad892d6c32456f8c3
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-02-25 10:27:02 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -515,9 +515,7 @@ class PropertyController extends Controller
 
         // $FormatJson = json_decode($recupererDonnéees, true);
         $description =
-            "<!-- wp:paragraph -->
-            <p>" . $request->description . "</p>
-            <!-- /wp:paragraph -->";
+            "<!-- wp:paragraph --> <p>" . $request->description . "</p> <!-- /wp:paragraph -->";
 
         $propertyData = [
             'title'   => $request->title,
