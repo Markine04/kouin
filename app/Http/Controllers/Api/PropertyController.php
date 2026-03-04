2d7ab7f453960e9004a36f6238a534c132e344cf
Prince Kouin <76432561+Markine04@users.noreply.github.com>
2026-03-04 10:08:35 +0000

    Update PropertyController.php

--- app/Http/Controllers/Api/PropertyController.php
+++ app/Http/Controllers/Api/PropertyController.php
@@ -632,9 +632,9 @@ class PropertyController extends Controller
             $queryParams['property-neighborhood'] = $neighborhood;
         }
 
-        if ($price) {
-            $queryParams['property-price'] = $price;
-        }
+        // if ($price) {
+        //     $queryParams['property-price'] = $price;
+        // }
 
         // ⚠️ WordPress ne filtre pas directement par price meta
         // Donc on ne met PAS property-price ici
